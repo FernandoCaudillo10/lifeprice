915bc31d251b11e59649f488445a47cefc8fed7f
Andres Gonzalez <jgonzalez-perez@csumb.edu>
2019-05-03 23:20:15 +0000

    started working on stuff

--- membersArea.php
+++ membersArea.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html>
+    <title>LifePrice</title>
+    <head>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>    
+    </head>
+    
+    
     hello hello and hello and hi/ca and cat and cat
     
     pew dsfhhdssdfs
