247f4109ea8dd4b056f37fd78385b824489ee76b
Giovanny Cerna <gcerna@csumb.edu>
2019-05-08 20:12:46 +0000

    Styling

--- doctorUpdate/company.php
+++ doctorUpdate/company.php
@@ -6,11 +6,22 @@
         <script src="https://code.jquery.com/jquery-3.1.1.min.js"> </script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../static/main.css" type="text/css" />
        
     </head>
     
     <body>
-     
+          
+          <header>
+            <div id="pageTitle">
+                <a href="index.php" class="removeLinkDecor"><h1>LifePrice</h1></a>
+            </div>
+             <div id="pageMenu">
+                <a href="../logout/logout.php" class="pageMenuItem removeLinkDecor"><div>Logout</div></a>
+             </div>
+            
+          </header>
+
     <form id="frm1">
       Enter company name: <input type="text" id = "company" name="company" value=""> 
       <br><br>
@@ -22,7 +33,6 @@
         
     </div>
        
-        <button id="logout" onclick="window.location.href = '../logout/logout.php';" type="button">logout</button>
     <footer class="footer">
         <div>LifePrice &copy; 2019</div>
         <div>F. Caudillo | C. Aldrete | G. Cerna | A. Gonzalez</div>
