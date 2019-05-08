06ede20e19468c52cf3e414ac559d4d8ede93067
Spaggety <andresjoseis@gmail.com>
2019-05-08 20:47:31 +0000

    added a delete account button

---
+++ LogInInfo/includesphp/del.php
@@ -0,0 +1,53 @@
+<?php
+include "../getDbconnect.php";
+
+if(isset($_POST["signDel"])){
+    $conn = getDataBaseConnection("lifePriceData");
+    $username = $_POST["email"];
+    $password = $_POST["passwordU"];
+    if(empty($username) || empty($password)){
+        header("Location: ../deleteAccount.php?error=emptyfields");
+        exit();
+    }
+    
+    $options = [
+    'cost' => 11,
+    //'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+    //'salt' => generateBase62String(22),
+    ];
+
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT, $options);
+
+    $sql = "SELECT * FROM userLoginInfo " .
+       "WHERE email = :username ";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array (":username" => $username));
+    $record = $stmt->fetch();
+    $isAuthenticated = password_verify($password, $record["password"]);
+
+    $use = json_encode($record);
+    
+    $someArray = json_decode($use, true);
+    if($isAuthenticated){
+        $id = $someArray[0];
+        
+        $sql = "DELETE FROM userLoginInfo WHERE Id=$id";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        
+        header("Location: ../index.php?delete=success");
+        exit();
+    }
+    else{
+        header("Location: ./LogInInfo/deleteAccount.php?error=noAcc");
+        session_destroy();
+        exit();
+    }
+    
+
+    
+}
+
+
+?>
