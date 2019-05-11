458f040866ff96254181c028a45f5982ebb9d483
Spaggety <andresjoseis@gmail.com>
2019-05-11 03:28:42 +0000

    store and display the symptoms

--- endpoints/dbConn.php
+++ endpoints/dbConn.php
@@ -1,8 +1,9 @@
 <?php
-    function getConnection($dbname = 'lifePriceDb'){
-        $host = "localhost";
-        $username = "root";
-        $password = "";
+function getConnection($dbname = 'lifePriceDb'){
+    $host = 'localhost';
+    $username= "root";
+    $password = '';
+    
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
@@ -10,13 +11,10 @@
         $username = $url["user"];
         $password = $url["pass"];
     } 
-        
-        //creates connection
-        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-        
-        //displays errors
-        $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        return $dbConn;
-    }
+    
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConn;
+    
+}
 ?>
